bb7c2471e7bdaf25e15077e9f09ece04d64236de
gaozifan <gzf222ash@gmail.com>
2019-07-17 03:34:58 +0800

    added new files

--- src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
+++ src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
@@ -1,4 +1,19 @@
 package ca.jrvs.apps.trading.dao;
 
+import org.apache.http.conn.HttpClientConnectionManager;
+
+import java.util.List;
+
 public class MarketDataDao {
+
+    public MarketDataDao(HttpClientConnectionManager clientConnectionManager){
+    }
+
+/*    public List<IexQuote> findIexQuoteByTicker(List<String> s){
+        return null;
+    }
+
+    public IexQuote findIexQupteByTicker(String s){
+        return null;
+    }*/
 }
