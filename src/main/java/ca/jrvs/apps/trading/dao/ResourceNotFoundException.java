0465592ee75a21a41531d6241e7a1f8ab030c3b8
gaozifan <gzf222ash@gmail.com>
2019-07-18 03:18:44 +0800

    wrote marketDataDao

--- src/main/java/ca/jrvs/apps/trading/dao/ResourceNotFoundException.java
+++ src/main/java/ca/jrvs/apps/trading/dao/ResourceNotFoundException.java
@@ -1,4 +1,11 @@
 package ca.jrvs.apps.trading.dao;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public ResourceNotFoundException(String msg, Exception ex){
+        super(msg, ex);
+    }
 }
